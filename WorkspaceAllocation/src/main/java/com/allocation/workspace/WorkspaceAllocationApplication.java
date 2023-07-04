e9c15efea9c8806f970c19cfc8c290113f93dbc7
nbagade <niranjan.bagade@nice.com>
2023-07-04 17:09:36 +0530

    first commit

---
+++ WorkspaceAllocation/src/main/java/com/allocation/workspace/WorkspaceAllocationApplication.java
@@ -0,0 +1,13 @@
+package com.allocation.workspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkspaceAllocationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkspaceAllocationApplication.class, args);
+	}
+
+}
---
+++ WorkspaceAllocation/src/test/java/com/allocation/workspace/WorkspaceAllocationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allocation.workspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkspaceAllocationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
